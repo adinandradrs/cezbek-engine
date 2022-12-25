b40bf6f0c85c4d6eda364d31b808b564399d5cff
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-26 02:39:42 +0700

    Update config to read use viper instead of godotenv

--- internal/apps/config.go
+++ internal/apps/config.go
@@ -2,22 +2,25 @@ package apps
 
 import (
 	"github.com/adinandradrs/cezbek-engine/internal/model"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"os"
 )
 
-func NewEnv(logger *zap.Logger) *model.TechnicalError {
+func NewEnv(logger *zap.Logger) (*model.TechnicalError, *viper.Viper) {
+	v := viper.New()
 	if _, err := os.Stat(".env"); err == nil {
-		err := godotenv.Load()
+		v.SetConfigFile(".env")
+		err := v.ReadInConfig()
 		if err != nil {
-			logger.Error("failed to load configuration from .env file", zap.Error(err))
+			logger.Fatal("failed to load configuration from .env file", zap.Error(err))
 			return &model.TechnicalError{
 				Exception: err.Error(),
-			}
+			}, nil
 		}
 	} else {
 		logger.Info("no configuration from .env")
+		v.AutomaticEnv()
 	}
-	return nil
+	return nil, v
 }
