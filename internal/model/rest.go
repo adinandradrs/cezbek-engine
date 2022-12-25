7fc03f60cd947a2ebce860ebb34be349f07f11c5
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-26 01:02:39 +0700

    Add body main components for rest response

---
+++ internal/model/rest.go
@@ -0,0 +1,13 @@
+package model
+
+type (
+	Response struct {
+		Data interface{} `json:"data,omitempty"`
+		Meta Meta        `json:"meta,omitempty"`
+	}
+
+	Meta struct {
+		Code    string `json:"code,omitempty"`
+		Message string `json:"message,omitempty"`
+	}
+)
