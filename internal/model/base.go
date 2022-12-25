73d15e8ca9058bd765d041f1d94507734b62b5a8
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-26 01:00:45 +0700

    Add model to initialize apps base entity

---
+++ internal/model/base.go
@@ -0,0 +1,22 @@
+package model
+
+import "database/sql"
+
+type BaseEntity struct {
+	IsDeleted   bool           `json:"is_deleted"`
+	CreatedBy   sql.NullString `json:"created_by"`
+	CreatedDate sql.NullTime   `json:"created_date"`
+	UpdatedBy   sql.NullString `json:"updated_by"`
+	UpdatedDate sql.NullTime   `json:"updated_date"`
+}
+
+type BusinessError struct {
+	ErrorCode    string
+	ErrorMessage string
+}
+
+type TechnicalError struct {
+	Exception string `json:"exception"`
+	Occurred  int64  `json:"occurred_unixts"`
+	Ticket    string `json:"ticket"`
+}
