017f639c14ad07f8558afcdc93c85c931d732815
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-26 01:19:26 +0700

    Add session request

--- internal/model/rest.go
+++ internal/model/rest.go
@@ -33,3 +33,28 @@ type (
 		TransactionId        string `json:"transaction_id"`
 	}
 )
+
+type (
+	SessionRequest struct {
+		Id       string `swaggerignore:"true"`
+		Partner  string `swaggerignore:"true"`
+		Username string `swaggerignore:"true"`
+		Msisdn   string `swaggerignore:"true"`
+		Email    string `swaggerignore:"true"`
+		Role     string `swaggerignore:"true"`
+		Fullname string `swaggerignore:"true"`
+		ContextRequest
+	}
+
+	ContextRequest struct {
+		Channel       string `json:"channel" swaggerignore:"true"`
+		OS            string `json:"os" swaggerignore:"true"`
+		Version       string `json:"version" swaggerignore:"true"`
+		Language      string `json:"language" swaggerignore:"true"`
+		DeviceId      string `json:"deviceId" swaggerignore:"true"`
+		Authorization string `json:"authorization" swaggerignore:"true"`
+		RefreshToken  string `json:"refreshToken" swaggerignore:"true"`
+		TransactionId string `json:"transactionId" swaggerignore:"true"`
+		ApiKey        string `json:"apiKey" swaggerignore:"true"`
+	}
+)
