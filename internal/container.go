c59f17671b43eeeb5e488052c61563aa94b63f47
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-31 20:21:12 +0700

    Update all config attribute to use hierarchy as JSON on Consul

--- internal/container.go
+++ internal/container.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"encoding/json"
 	"github.com/adinandradrs/cezbek-engine/internal/adaptor"
 	"github.com/adinandradrs/cezbek-engine/internal/apps"
 	"github.com/adinandradrs/cezbek-engine/internal/repository"
@@ -9,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/shopspring/decimal"
 	"github.com/spf13/viper"
@@ -30,8 +32,7 @@ type (
 
 	Infra struct {
 		adaptor.S3Watcher
-		CiamKTM       adaptor.Cognito
-		CiamTraveller adaptor.Cognito
+		CiamPartner adaptor.CiamWatcher
 	}
 
 	Dao struct {
@@ -75,11 +76,11 @@ func NewContainer(app string) Container {
 func (c *Container) loadPool() *storage.PgPool {
 	opts := c.Viper.GetString("db_options")
 	return storage.NewPgPool(&storage.PgOptions{
-		Host:    c.Viper.GetString("db_host"),
-		Port:    c.Viper.GetString("db_port"),
-		User:    c.Viper.GetString("db_username"),
-		Passwd:  c.Viper.GetString("db_password"),
-		Schema:  c.Viper.GetString("db_schema"),
+		Host:    c.Viper.GetString("db.host"),
+		Port:    c.Viper.GetString("db.port"),
+		User:    c.Viper.GetString("db.username"),
+		Passwd:  c.Viper.GetString("db.password"),
+		Schema:  c.Viper.GetString("db.schema"),
 		Options: &opts,
 		Logger:  c.Logger,
 	})
@@ -93,26 +94,46 @@ func (c *Container) registerRepository() Dao {
 }
 
 func (c *Container) LoadInfra() Infra {
-	kid, skey := c.Viper.GetString("aws_keyid"), c.Viper.GetString("aws_secretkey")
+	kid, skey := c.Viper.GetString("aws.keyid"), c.Viper.GetString("aws.keysecret")
 	s3Credential, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(c.Viper.GetString("aws_s3_region")),
+		Region:      aws.String(c.Viper.GetString("aws.s3.region")),
 		Credentials: credentials.NewStaticCredentials(kid, skey, ""),
 	})
-	s3Watcher := adaptor.NewS3(&adaptor.S3Bucket{
-		Bucket:   c.Viper.GetString("aws_s3_bucket"),
+	s3Watcher := adaptor.NewS3(adaptor.S3Bucket{
+		Bucket:   c.Viper.GetString("aws.s3.bucket"),
 		Uploader: s3manager.NewUploader(s3Credential),
 		Logger:   c.Logger,
 	})
-
+	jwkb, _ := json.Marshal(c.Viper.Get("aws.ciam.partner.jwk"))
+	jwk := string(jwkb)
+	ciamPartner := adaptor.NewCognito(adaptor.Cognito{
+		Provider: c.LoadCognito(c.Viper.GetString("aws.ciam.region")),
+		ClientId: c.Viper.GetString("aws.ciam.partner.clientid"),
+		UserPool: c.Viper.GetString("aws.ciam.partner.poolid"),
+		Scrt:     c.Viper.GetString("aws.ciam.partner.secret"),
+		Region:   c.Viper.GetString("aws.ciam.region"),
+		JWK:      jwk,
+		Logger:   c.Logger,
+	})
 	return Infra{
-		S3Watcher: s3Watcher,
+		S3Watcher:   s3Watcher,
+		CiamPartner: ciamPartner,
 	}
 }
 
+func (c *Container) LoadCognito(region string) *cognito.CognitoIdentityProvider {
+	cfg := &aws.Config{Region: aws.String(region)}
+	s, err := session.NewSession(cfg)
+	if err != nil {
+		c.Logger.Panic("failed to load CIAM cognito", zap.Error(err))
+	}
+	return cognito.New(s)
+}
+
 func (c *Container) LoadRedis() (rds storage.Cacher) {
-	addrs := strings.Split(c.Viper.GetString("rds_addresses"), ";")
-	pwd := c.Viper.GetString("rds_password")
-	if c.Viper.GetBool("rds_cluster") {
+	addrs := strings.Split(c.Viper.GetString("rds.addresses"), ";")
+	pwd := c.Viper.GetString("rds.password")
+	if c.Viper.GetBool("rds.cluster") {
 		rds = storage.NewClusterRedis(&storage.RedisOptions{
 			Addrs:  addrs,
 			Passwd: pwd,
@@ -131,11 +152,12 @@ func (c *Container) LoadRedis() (rds storage.Cacher) {
 
 func (c *Container) RegisterUsecase(infra Infra, rds storage.Cacher) Usecase {
 	dao := c.registerRepository()
-	cdn := c.Viper.GetString("aws_cdn_base")
-	path := c.Viper.GetString("s3_path")
+	cdn := c.Viper.GetString("aws.cdn_base")
+	path := c.Viper.GetString("aws.s3.path")
 	return Usecase{
 		PartnerManager: management.NewPartner(management.Partner{
 			Dao:       dao,
+			Ciam:      infra.CiamPartner,
 			S3Watcher: infra.S3Watcher,
 			CDN:       cdn,
 			PathS3:    path,
