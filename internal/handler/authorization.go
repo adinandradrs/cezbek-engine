9c4223d1e5c990a091f08d54c1a45e8248d25813
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2023-01-01 03:51:17 +0700

    Add validate required on client auth request

--- internal/handler/authorization.go
+++ internal/handler/authorization.go
@@ -18,6 +18,7 @@ func newAuthorizationResource(a Authorization) *Authorization {
 func AuthorizationHandler(r fiber.Router, a Authorization) {
 	h := newAuthorizationResource(a)
 	r.Post("/client", h.clientAuth)
+	r.Post("/b2b", h.b2bAuth)
 }
 
 // @Tags Authorization APIs
@@ -43,6 +44,47 @@ func (a *Authorization) clientAuth(ctx *fiber.Ctx) error {
 	inp := model.ClientAuthenticationRequest{}
 	if err := ctx.BodyParser(&inp); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(err)
+	}
+	bad := apps.ValidateStruct(checker.Struct(inp))
+	if bad != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(bad)
+	}
+	inp.ApiKey = ctx.Get(apps.HeaderApiKey)
+	v, ex := a.AuthenticateClient(&inp)
+	if ex != nil && ex.ErrorCode == apps.ErrCodeUnauthorized {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(apps.BusinessErrorResponse(ex))
+	}
+	if ex != nil && ex.ErrorCode == apps.ErrCodeSomethingWrong {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(apps.BusinessErrorResponse(ex))
+	}
+	return ctx.JSON(apps.DefaultSuccessResponse(apps.SuccessMsgSubmit, v))
+}
+
+// @Tags Authorization APIs
+// Client Authorization API
+// @Summary B2B Partner Authorization API
+// @Description This API is to authorize B2B partner's officer account
+// @Schemes
+// @Accept json
+// @Param x-client-channel header string true "Client Channel" Enums(EBIZKEZBEK, B2BCLIENT)
+// @Param x-client-os  header string true "Client OS or Browser Agent" default(android 10)
+// @Param x-client-device  header string true "Client Device ID"
+// @Param x-client-version  header string true "Client Platform Version" default(1.0.0)
+// @Param x-client-timestamp  header string true "Client Original Timestamp in UNIX format (EPOCH)"
+// @Param request body model.OfficerAuthenticationRequest true "B2B Partner Authentication Payload"
+// @Success 200
+// @Failure 400
+// @Failure 401
+// @Failure 500
+// @Router /v1/authorization/b2b [post]
+func (a *Authorization) b2bAuth(ctx *fiber.Ctx) error {
+	inp := model.ClientAuthenticationRequest{}
+	if err := ctx.BodyParser(&inp); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(err)
+	}
+	bad := apps.ValidateStruct(checker.Struct(inp))
+	if bad != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(bad)
 	}
 	inp.ApiKey = ctx.Get(apps.HeaderApiKey)
 	v, ex := a.AuthenticateClient(&inp)
