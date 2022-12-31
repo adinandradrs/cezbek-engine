ffca490e2920e4e3f0e94a289b102aa38ae82663
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-31 20:38:32 +0700

    Update logic to integrate add partner with CIAM

--- internal/handler/partner.go
+++ internal/handler/partner.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/adinandradrs/cezbek-engine/internal/apps"
+	"github.com/adinandradrs/cezbek-engine/internal/model"
 	"github.com/adinandradrs/cezbek-engine/internal/usecase/management"
 	"github.com/gofiber/fiber/v2"
 )
@@ -26,7 +27,7 @@ func PartnerHandler(r fiber.Router, p Partner) {
 // @Schemes
 // @Accept json
 // @Param Authorization header string false "Your Token to Access" default(Bearer )
-// @Param x-client-channel header string true "Client Channel" Enums(EBIZKEZBEK)
+// @Param x-client-channel header string true "Client Channel" Enums(EBIZKEZBEK, B2BCLIENT)
 // @Param x-client-os  header string true "Client OS or Browser Agent" default(android 10)
 // @Param x-client-device  header string true "Client Device ID"
 // @Param x-client-version  header string true "Client Platform Version" default(1.0.0)
@@ -36,18 +37,44 @@ func PartnerHandler(r fiber.Router, p Partner) {
 // @Param email formData string true "Partner Email" default(kezbek.support@lajada.net)
 // @Param msisdn formData string true "MSISDN" default(628123456789)
 // @Param officer formData string true "Partner Officer" default(John Doe)
-// @Param address formData string true "Office Address" default(100000)
+// @Param address formData string true "Office Address" default(Bintaro Exchange Mall Blok A1)
 // @Param logo formData file true "Logo"
 // @Success 200
 // @Failure 400
 // @Failure 401
 // @Failure 403
 // @Failure 500
+// @Failure 503
 // @Router /v1/partners [post]
 func (p *Partner) add(ctx *fiber.Ctx) error {
-	v, ex := p.Add(nil)
-	if ex != nil && ex.ErrorCode == apps.ErrCodeBussInvalidCodePartner {
-		return ctx.Status(fiber.StatusBadRequest).JSON(ex)
+	logo, _ := ctx.FormFile("logo")
+	inp := model.AddPartnerRequest{
+		Partner: ctx.FormValue("partner"),
+		Code:    ctx.FormValue("code"),
+		Email:   ctx.FormValue("email"),
+		Msisdn:  ctx.FormValue("msisdn"),
+		Officer: ctx.FormValue("officer"),
+		Address: ctx.FormValue("address"),
+		Logo:    *logo,
+		SessionRequest: model.SessionRequest{
+			Id: 0,
+		},
 	}
+	bad := apps.ValidateStruct(checker.Struct(inp))
+	if bad != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(bad)
+	}
+	v, ex := p.Add(&inp)
+
+	if ex != nil && ex.ErrorCode == apps.ErrCodeBussPartnerExists {
+		return ctx.Status(fiber.StatusBadRequest).JSON(apps.BusinessErrorResponse(ex))
+	}
+	if ex != nil && ex.ErrorCode == apps.ErrCodeESBUnavailable {
+		return ctx.Status(fiber.StatusServiceUnavailable).JSON(apps.BusinessErrorResponse(ex))
+	}
+	if ex != nil && (ex.ErrorCode == apps.ErrCodeSubmitted || ex.ErrorCode == apps.ErrCodeSomethingWrong) {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(apps.BusinessErrorResponse(ex))
+	}
+
 	return ctx.JSON(apps.DefaultSuccessResponse(apps.SuccessMsgSubmit, v))
 }
--- internal/usecase/management/partner.go
+++ internal/usecase/management/partner.go
@@ -16,10 +16,11 @@ import (
 
 type Partner struct {
 	Dao       repository.PartnerPersister
-	Logger    *zap.Logger
+	Ciam      adaptor.CiamWatcher
 	S3Watcher adaptor.S3Watcher
 	CDN       string
 	PathS3    string
+	Logger    *zap.Logger
 }
 
 type PartnerManager interface {
@@ -46,58 +47,87 @@ func (p Partner) uploadLogo(tid string, logo multipart.FileHeader) (*string, *mo
 	return &floc, nil
 }
 
-func (p Partner) generateSecret(inp *model.AddPartnerRequest) (*string, []byte, *model.TechnicalError) {
+func (p Partner) generateSecret(inp *model.AddPartnerRequest, pass *string) (*string, []byte, *model.TechnicalError) {
 	salt := apps.Hash(inp.Code + ":" + uuid.NewString())
-	gpass, ex := apps.RandomPassword(12, 5, 3, p.Logger)
-	if ex != nil {
-		return nil, nil, ex
-	}
-	secret, ex := apps.Encrypt(gpass, salt, p.Logger)
+	secret, ex := apps.Encrypt(*pass, salt, p.Logger)
 	if ex != nil {
 		return nil, nil, ex
 	}
 	return &salt, secret, nil
 }
 
+func (p Partner) ciamRegistration(data *model.Partner, pass *string) (*model.CiamUserResponse, *model.BusinessError) {
+	resp, ex := p.Ciam.OnboardPartner(model.CiamOnboardPartnerRequest{
+		Email:       data.Email.String,
+		PhoneNumber: data.Msisdn.String,
+		Username:    data.Code.String,
+		Name:        data.Partner.String,
+		Picture:     data.Logo.String,
+		Password:    *pass,
+	})
+	if ex != nil {
+		return nil, &model.BusinessError{
+			ErrorCode:    apps.ErrCodeESBUnavailable,
+			ErrorMessage: apps.ErrMsgESBUnavailable,
+		}
+	}
+	return resp, nil
+}
+
 func (p Partner) Add(inp *model.AddPartnerRequest) (*model.TransactionResponse, *model.BusinessError) {
 	tid := apps.TransactionId(inp.Code + apps.DefaultTrxId)
 	data := model.Partner{
-		Code:   sql.NullString{String: inp.Code, Valid: true},
-		Email:  sql.NullString{String: inp.Email, Valid: true},
-		Msisdn: sql.NullString{String: inp.Msisdn, Valid: true},
+		Officer: sql.NullString{String: inp.Officer, Valid: true},
+		Partner: sql.NullString{String: inp.Partner, Valid: true},
+		Code:    sql.NullString{String: inp.Code, Valid: true},
+		Email:   sql.NullString{String: inp.Email, Valid: true},
+		Msisdn:  sql.NullString{String: inp.Msisdn, Valid: true},
+		Status:  apps.StatusActive,
+		BaseEntity: model.BaseEntity{
+			CreatedBy: sql.NullInt64{Int64: inp.SessionRequest.Id, Valid: true},
+		},
 	}
 
 	count, err := p.Dao.CountByIdentifier(data)
-	if *count == 0 || err != nil {
+	if *count > 0 || err != nil {
 		return nil, &model.BusinessError{
 			ErrorCode:    apps.ErrCodeBussPartnerExists,
 			ErrorMessage: apps.ErrMsgBussPartnerExists,
 		}
 	}
 
-	floc, err := p.uploadLogo(tid, inp.Logo)
-	if err != nil {
-		return nil, &model.BusinessError{
-			ErrorCode:    apps.ErrCodeSomethingWrong,
-			ErrorMessage: apps.ErrMsgSomethingWrong,
-		}
-	}
-
-	salt, secret, ex := p.generateSecret(inp)
+	gpass, ex := apps.RandomPassword(12, 5, 3, p.Logger)
+	p.Logger.Info("generate password", zap.String("code", inp.Code), zap.String("genpass", gpass))
 	if ex != nil {
 		return nil, &model.BusinessError{
 			ErrorCode:    apps.ErrCodeSomethingWrong,
 			ErrorMessage: apps.ErrMsgSomethingWrong,
 		}
 	}
-
-	data.Partner = sql.NullString{String: inp.Partner, Valid: true}
-	data.ApiKey = sql.NullString{String: apps.Hash(inp.Code + uuid.NewString() + inp.Email), Valid: true}
+	salt, secret, ex := p.generateSecret(inp, &gpass)
+	if ex != nil {
+		return nil, &model.BusinessError{
+			ErrorCode:    apps.ErrCodeSomethingWrong,
+			ErrorMessage: apps.ErrMsgSomethingWrong,
+		}
+	}
 	data.Salt = sql.NullString{String: *salt, Valid: true}
 	data.Secret = secret
-	data.Officer = sql.NullString{String: inp.Officer, Valid: true}
-	data.PartnerLogo = sql.NullString{String: *floc, Valid: true}
-	data.Status = apps.StatusActive
+
+	floc, ex := p.uploadLogo(tid, inp.Logo)
+	if ex != nil {
+		return nil, &model.BusinessError{
+			ErrorCode:    apps.ErrCodeSomethingWrong,
+			ErrorMessage: apps.ErrMsgSomethingWrong,
+		}
+	}
+	data.Logo = sql.NullString{String: *floc, Valid: true}
+
+	ciam, cex := p.ciamRegistration(&data, &gpass)
+	if cex != nil {
+		return nil, cex
+	}
+	data.ApiKey = sql.NullString{String: apps.Hash(inp.Code + uuid.NewString() + ciam.SubId), Valid: true}
 	ex = p.Dao.Add(data)
 	if ex != nil {
 		return nil, &model.BusinessError{
