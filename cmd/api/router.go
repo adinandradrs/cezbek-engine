08169056550f36817092bfc7530839eb777e4762
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2023-01-01 03:51:42 +0700

    Remove logging for signature client

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -7,10 +7,6 @@ import (
 	"github.com/adinandradrs/cezbek-engine/internal/handler"
 	"github.com/gofiber/fiber/v2"
 	swagger "github.com/swaggo/fiber-swagger"
-	"go.uber.org/zap"
-	"strconv"
-	"strings"
-	"time"
 )
 
 // @title Kezbek - Cashback Engine Sandbox
@@ -49,12 +45,6 @@ func main() {
 	handler.PartnerHandler(partners, handler.Partner{
 		PartnerManager: ucase.PartnerManager,
 	})
-	//hmac
-	epoch := time.Now().Unix()
-
-	d := fiber.MethodPost + ":" + strings.ToUpper("LAJADA") + ":" + strconv.FormatInt(epoch, 10) + ":" +
-		strings.ToUpper("ee33c45e2cfe3e08d352698d31da6bee")
-	c.Logger.Info("EPOCH", zap.Int64("unixts", epoch), zap.String("hmac", apps.HMAC(d, "LAJADA")))
 
 	_ = app.Listen(env.HttpPort)
 }
--- internal/apps/middleware.go
+++ internal/apps/middleware.go
@@ -20,46 +20,71 @@ func unauthorized(ctx *fiber.Ctx) error {
 	})
 }
 
+func isSignedUrl(signs *[]string, ctx *fiber.Ctx) bool {
+	isSign := false
+	for _, s := range *signs {
+		if strings.Contains(string(ctx.Request().URI().Path()), s) {
+			isSign = true
+			break
+		}
+	}
+	return isSign
+}
+
+func validateNonSignedAccess(ctx *fiber.Ctx) error {
+	if ctx.Get(HeaderClientChannel) != ChannelEBizKezbek {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(model.Response{
+			Meta: model.Meta{
+				Code:    ErrCodeInvalidChannel,
+				Message: ErrMsgInvalidChannel,
+			},
+		})
+	}
+	return nil
+}
+
+func validateSignedAccess(ctx *fiber.Ctx) (*string, error) {
+	if ctx.Get(HeaderClientChannel) != ChannelB2BClient {
+		return nil, ctx.Status(fiber.StatusUnauthorized).JSON(model.Response{
+			Meta: model.Meta{
+				Code:    ErrCodeInvalidChannel,
+				Message: ErrMsgInvalidChannel,
+			},
+		})
+	}
+	b := struct {
+		Code string `json:"code"`
+	}{}
+	if err := ctx.BodyParser(&b); err != nil {
+		return nil, ctx.Status(fiber.StatusBadRequest).JSON(err)
+	}
+	return &b.Code, nil
+}
+
 func Authenticator(m Middleware) fiber.Handler {
 	signs := []string{
 		"/api/v1/authorization/client",
 	}
 	return func(ctx *fiber.Ctx) error {
-		isSign := false
-		for _, s := range signs {
-			if strings.Contains(string(ctx.Request().URI().Path()), s) {
-				isSign = true
-				break
+		if isSignedUrl(&signs, ctx) {
+			code, err := validateSignedAccess(ctx)
+			if err != nil {
+				return err
 			}
-		}
-
-		if isSign {
-			if ctx.Get(HeaderClientChannel) != ChannelB2BClient {
-				return ctx.Status(fiber.StatusUnauthorized).JSON(model.Response{
-					Meta: model.Meta{
-						Code:    ErrCodeInvalidChannel,
-						Message: ErrMsgInvalidChannel,
-					},
-				})
-			}
-			b := struct {
-				Code string `json:"code"`
-			}{}
-			if err := ctx.BodyParser(&b); err != nil {
-				return ctx.Status(fiber.StatusBadRequest).JSON(err)
-			}
-
 			epoch := ctx.Get(HeaderClientTimestamp)
 			key := ctx.Get(HeaderApiKey)
 
-			d := string(ctx.Request().Header.Method()) + ":" + strings.ToUpper(b.Code) + ":" + epoch + ":" + strings.ToUpper(key)
-			if HMAC(d, b.Code) != ctx.Get(HeaderClientSignature) {
+			d := string(ctx.Request().Header.Method()) + ":" + strings.ToUpper(*code) + ":" + epoch + ":" + strings.ToUpper(key)
+			if HMAC(d, *code) != ctx.Get(HeaderClientSignature) {
 				m.Logger.Error("the given signature is not recognized", zap.String("signature", ctx.Get(HeaderClientSignature)),
-					zap.String("code", b.Code))
+					zap.String("code", *code))
 				return unauthorized(ctx)
 			}
 		}
-
+		err := validateNonSignedAccess(ctx)
+		if err != nil {
+			return err
+		}
 		return ctx.Next()
 	}
 }
