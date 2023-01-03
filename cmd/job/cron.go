68cc140d3ef8ef65bf01a802fb6db14e4bbce58f
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2023-01-04 00:20:30 +0700

    Update var name for cmd job as the application name

--- cmd/job/cron.go
+++ cmd/job/cron.go
@@ -10,14 +10,14 @@ func main() {
 	c := cdi.NewContainer("app_cezbek_job")
 	infra := c.LoadInfra()
 	ucase := c.RegisterJobUsecase(infra)
-	s := gocron.NewScheduler(time.UTC)
+	job := gocron.NewScheduler(time.UTC)
 	c.Logger.Info("cezbek cron job is running on background...")
-	_, err := s.Every(c.Viper.GetString("schedule.send_otp_email")).Do(func() {
+	_, err := job.Every(c.Viper.GetString("schedule.send_otp_email")).Do(func() {
 		_ = ucase.JobOnboardWatcher.SendOtpEmail()
 	})
 	if err != nil {
 		c.Logger.Panic("cezbek cron job is failing to run [JobOnboardManager.SendOtpEmail]")
 	}
 
-	s.StartBlocking()
+	job.StartBlocking()
 }
